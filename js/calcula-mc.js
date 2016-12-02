26efc4555c4da61476b7591a2705fe0c2c80b996
rafaelavm <rafaela.vm@gmail.com>
2016-12-02 17:45:50 -0200

    2. Variáveis e comandos básicos

---
+++ js/calcula-mc.js
@@ -0,0 +1,12 @@
+// peso / altura * altura
+var peso = 64;
+var altura = 1.67;
+
+if (altura != 0) {
+    var imc = peso / (altura * altura);
+    console.log(imc);
+}
+else{
+
+    console.log("A altura é igual a zero");
+}
