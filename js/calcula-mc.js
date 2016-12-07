391ef5ac2a58b519862d828cf9a560805abd0896
Rafaela <rafaela.vm@gmail.com>
2016-12-07 16:57:18 -0200

    3. Conhecendo o DOM e organizando nosso código com objetos  (2)

--- js/calcula-mc.js
+++ js/calcula-mc.js
@@ -1,21 +1,26 @@
 var tdPeso = document.getElementById('peso-2');
 var tdAltura = document.getElementById('altura-2');
 
-//textContent pega o peso
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+//textContent pega o texto (peso e altura)
+var paciente = {
+    peso: tdPeso.textContent,
+    altura: tdAltura.textContent
+}
+
+//pra pegar o valor existem duas formas:
+//paciente.peso;
+//paciente['peso'];
 
 // peso / altura * altura
 
-if (altura != 0) {
-    var imc = peso / (altura * altura);
+if (paciente.altura != 0) {
+    var imc = paciente.peso / (paciente.altura * paciente.altura);
 
     var tdIMC = document.getElementById('imc-2');
     tdIMC.textContent = imc;
 
     console.log(imc);
-}
-else{
+} else {
 
     console.log("A altura é igual a zero");
 }
