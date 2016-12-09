ac8fa4ebc2f0ff9229ece9081b42ea817fbf4b04
Rafaela <rafaela.vm@gmail.com>
2016-12-09 18:37:37 -0200

    4. Coleções e loops (1)

--- js/calcula-mc.js
+++ js/calcula-mc.js
@@ -1,26 +1,55 @@
-var tdPeso = document.getElementById('peso-2');
-var tdAltura = document.getElementById('altura-2');
+var tdPeso2 = document.getElementById('peso-2');
+var tdAltura2 = document.getElementById('altura-2');
+
+var peso2 = tdPeso2.textContent;
+var altura2 = tdAltura2.textContent;
 
 //textContent pega o texto (peso e altura)
-var paciente = {
-    peso: tdPeso.textContent,
-    altura: tdAltura.textContent
+var paciente2 = {
+    peso: peso2,
+    altura: altura2
 }
 
 //pra pegar o valor existem duas formas:
 //paciente.peso;
 //paciente['peso'];
 
+var tdPeso1 = document.getElementById('peso-1');
+var tdAltura1 = document.getElementById('altura-1');
+
+var peso1 = tdPeso1.textContent;
+var altura1 = tdAltura1.textContent;
+
+//textContent pega o texto (peso e altura)
+var paciente1 = {
+    peso: peso1,
+    altura: altura1
+}
+
+var pacientes = [paciente2, paciente1];
+
+var primeiro = pacientes[0];
+var segundo = pacientes[1];
+
+var posicaoAtual = 0;
+
 // peso / altura * altura
 
-if (paciente.altura != 0) {
-    var imc = paciente.peso / (paciente.altura * paciente.altura);
+while (posicaoAtual <= pacientes.length - 1) {
 
-    var tdIMC = document.getElementById('imc-2');
-    tdIMC.textContent = imc;
+    if (pacientes[posicaoAtual].altura != 0) {
+        var imc = pacientes[posicaoAtual].peso / (pacientes[posicaoAtual].altura * pacientes[posicaoAtual].altura);
 
-    console.log(imc);
-} else {
+        var tdIMC = document.getElementById('imc-2');
 
-    console.log("A altura é igual a zero");
+        tdIMC.textContent = imc;
+
+        console.log(imc);
+    } else {
+
+        console.log("A altura é igual a zero");
+    }
+
+    posicaoAtual++;
+
 }
