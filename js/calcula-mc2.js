7090920e8bc8a7c433b784a400f0fa3ee2e18d85
Rafaela <rafaela.vm@gmail.com>
2016-12-14 20:00:52 -0200

    4. Coleções e loops (3): aplicando o for

--- js/calcula-mc2.js
+++ js/calcula-mc2.js
@@ -1,28 +1,28 @@
 //pegar todos os elementos dessa classe
-//array de trs
+//isso transforma o trsPacientes num array de trs
 var trsPacientes = document.getElementsByClassName('paciente');
-
-var posicaoAtual = 0;
 
 // peso / altura * altura
 
-while (posicaoAtual <= trsPacientes.length - 1) {
 
+for (var posicaoAtual = 0; posicaoAtual <= trsPacientes.length - 1; posicaoAtual++) {
+
+    //pega o tr correspondente e tudo que tem dentro dele
     var pacienteTr = trsPacientes[posicaoAtual];
+    console.log(pacienteTr);
+    console.log(pacienteTr.getElementsByClassName('info-nome'));
 
     //ele vai pegar dentro da tr a td com a classe 'info-xxx', que tb devolve um array
     var tdNome = pacienteTr.getElementsByClassName('info-nome')[0];
     var tdPeso = pacienteTr.getElementsByClassName('info-peso')[0];
     var tdAltura = pacienteTr.getElementsByClassName('info-altura')[0];
     var tdImc =
-
-    //pegar todos os dados acima e criar um objeto paciente
-    paciente = {
-        nome : tdNome.textContent,
-        peso: tdPeso.textContent,
-        altura: tdAltura.textContent
-    };
-
+        //pegar todos os dados acima e criar um objeto paciente
+        paciente = {
+            nome: tdNome.textContent,
+            peso: tdPeso.textContent,
+            altura: tdAltura.textContent
+        };
 
     if (paciente.altura != 0) {
 
@@ -38,7 +38,4 @@ while (posicaoAtual <= trsPacientes.length - 1) {
 
         console.log("A altura é igual a zero");
     }
-
-    posicaoAtual++;
-
 }
