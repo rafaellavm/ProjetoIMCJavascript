50436d52b7f4755d1d643cd925b50950facd82d9
Rafaela <rafaela.vm@gmail.com>
2016-12-16 12:08:06 -0200

    5. Funções e os cidadãos de primeira classe (1,2)

--- js/calcula-mc2.js
+++ js/calcula-mc2.js
@@ -1,41 +1,48 @@
+//função para calcular o imc
+// peso / altura * altura
+//função feita para ser chamada a partir de um objeto
+//função não pode ser global
+function calculaIMC() {}
+
 //pegar todos os elementos dessa classe
 //isso transforma o trsPacientes num array de trs
 var trsPacientes = document.getElementsByClassName('paciente');
-
-// peso / altura * altura
-
 
 for (var posicaoAtual = 0; posicaoAtual <= trsPacientes.length - 1; posicaoAtual++) {
 
     //pega o tr correspondente e tudo que tem dentro dele
     var pacienteTr = trsPacientes[posicaoAtual];
-    console.log(pacienteTr);
-    console.log(pacienteTr.getElementsByClassName('info-nome'));
 
     //ele vai pegar dentro da tr a td com a classe 'info-xxx', que tb devolve um array
     var tdNome = pacienteTr.getElementsByClassName('info-nome')[0];
     var tdPeso = pacienteTr.getElementsByClassName('info-peso')[0];
     var tdAltura = pacienteTr.getElementsByClassName('info-altura')[0];
+
     var tdImc =
         //pegar todos os dados acima e criar um objeto paciente
-        paciente = {
-            nome: tdNome.textContent,
-            peso: tdPeso.textContent,
-            altura: tdAltura.textContent
+        pacienteAtual = {
+            nome: tdNome.textContent
+            , peso: tdPeso.textContent
+            , altura: tdAltura.textContent
+
+            //a propriedade é uma função
+            //funcao anônima, função que não tem nome
+            , pegaImc: function () {
+                if (this.altura != 0) {
+                    var imc = this.peso / (this.altura * this.altura);
+                    return imc;
+                }
+                else {
+                    console.log("A altura é igual a zero");
+                }
+            }
         };
 
-    if (paciente.altura != 0) {
+    var imc = pacienteAtual.pegaImc();
 
-        var imc = paciente.peso / (paciente.altura * paciente.altura);
+    var tdIMC = pacienteTr.getElementsByClassName('info-imc')[0];
 
-        var tdIMC = pacienteTr.getElementsByClassName('info-imc')[0];
+    tdIMC.textContent = imc;
 
-        tdIMC.textContent = imc;
-
-        console.log(imc);
-
-    } else {
-
-        console.log("A altura é igual a zero");
-    }
+    console.log(imc);
 }
