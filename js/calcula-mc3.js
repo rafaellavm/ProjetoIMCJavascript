7ab8f1707e5189820f09c46084d32b762deee0d3
Rafaela <rafaela.vm@gmail.com>
2016-12-23 14:31:42 -0200

    6. Eventos (1)

--- js/calcula-mc3.js
+++ js/calcula-mc3.js
@@ -1,40 +1,52 @@
-//pegar todos os elementos dessa classe
-//isso transforma o trsPacientes num array de trs
-var trsPacientes = document.getElementsByClassName('paciente');
+function calculaTodosImcs() {
 
-percorreArray(trsPacientes,function(pacienteTr){
+    //pegar todos os elementos dessa classe
+    //isso transforma o trsPacientes num array de trs
+    var trsPacientes = document.getElementsByClassName('paciente');
 
-    //ele vai pegar dentro da tr a td com a classe 'info-xxx', que tb devolve um array
-    var tdNome = pacienteTr.getElementsByClassName('info-nome')[0];
-    var tdPeso = pacienteTr.getElementsByClassName('info-peso')[0];
-    var tdAltura = pacienteTr.getElementsByClassName('info-altura')[0];
+    percorreArray(trsPacientes, function (pacienteTr) {
 
-    var tdImc =
-        //pegar todos os dados acima e criar um objeto paciente
-        pacienteAtual = {
-            nome: tdNome.textContent
-            , peso: tdPeso.textContent
-            , altura: tdAltura.textContent
+        //ele vai pegar dentro da tr a td com a classe 'info-xxx', que tb devolve um array
+        var tdNome = pacienteTr.getElementsByClassName('info-nome')[0];
+        var tdPeso = pacienteTr.getElementsByClassName('info-peso')[0];
+        var tdAltura = pacienteTr.getElementsByClassName('info-altura')[0];
+        var tdImc =
 
-            //a propriedade é uma função
-            //funcao anônima, função que não tem nome
-            //é uma função que só serve pro paciente, não precisa ser global
-            , pegaImc: function () {
-                if (this.altura != 0) {
-                    var imc = this.peso / (this.altura * this.altura);
-                    return imc;
+            //pegar todos os dados acima e criar um objeto paciente
+            pacienteAtual = {
+                nome: tdNome.textContent
+                , peso: tdPeso.textContent
+                , altura: tdAltura.textContent
+                    //a propriedade é uma função
+                    //funcao anônima, função que não tem nome
+                    //é uma função que só serve pro paciente, não precisa ser global
+
+                , pegaImc: function () {
+                    if (this.altura != 0) {
+                        var imc = this.peso / (this.altura * this.altura);
+                        return imc;
+                    }
+                    else {
+                        console.log("A altura é igual a zero");
+                    }
                 }
-                else {
-                    console.log("A altura é igual a zero");
-                }
-            }
-        };
+            };
 
-    var imc = pacienteAtual.pegaImc();
+        var imc = pacienteAtual.pegaImc();
+        var tdIMC = pacienteTr.getElementsByClassName('info-imc')[0];
+        tdIMC.textContent = imc;
 
-    var tdIMC = pacienteTr.getElementsByClassName('info-imc')[0];
+        //console.log(imc);
+    });
+}
 
-    tdIMC.textContent = imc;
+var botao = document.getElementById('calcula-imcs');
 
-    console.log(imc);
+//'ouve' um evento
+botao.addEventListener('click',calculaTodosImcs);
+botao.addEventListener('click',function(){
+    console.log('Calculando imcs');
 });
+
+
+
